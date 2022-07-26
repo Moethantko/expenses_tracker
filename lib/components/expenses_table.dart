d9acd8e20738128ecb8244c85baae866a7a6e059
Moe Ko <mko23@wooster.edu>
2022-07-26 02:14:35 -0400

    stream data from firebase and display it with a table

--- lib/components/expenses_table.dart
+++ lib/components/expenses_table.dart
@@ -1,94 +1,75 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class ExpensesTable extends StatelessWidget {
   const ExpensesTable({Key? key}) : super(key: key);
 
+  /*void expensesDataStream () async {
+    await for (var snapshot in FirebaseFirestore.instance.collection('expenses').snapshots()) {
+      for (var expense in snapshot.docs) {
+        print(expense.data());
+      }
+    }
+  }*/
+
   @override
   Widget build(BuildContext context) {
     return Container(
-      margin: EdgeInsets.only(top: 40),
-      child: Table(
-        border: TableBorder.all(),
-        defaultVerticalAlignment: TableCellVerticalAlignment.middle,
-        children: <TableRow>[
-          TableRow(
-            decoration: const BoxDecoration(
-                color: Colors.white
-            ),
-            children: <Widget>[
-              TableHeader(headerName: "Category"),
-              TableHeader(headerName: "Price"),
-              TableHeader(headerName: "Date"),
-            ],
-          ),
-          TableRow(
-              children: <Widget>[
-                TableRowItem(itemName: "Food"),
-                TableRowItem(itemName: "${String.fromCharCodes(Runes('\u0024'))}300"),
-                TableRowItem(itemName: "July 24, 2022"),
-              ]
-          ),
-          TableRow(
-              children: <Widget>[
-                TableRowItem(itemName: "Movie"),
-                TableRowItem(itemName: "${String.fromCharCodes(Runes('\u0024'))}200"),
-                TableRowItem(itemName: "July 20, 2022"),
-              ]
-          ),
-          TableRow(
-              children: <Widget>[
-                TableRowItem(itemName: "Food"),
-                TableRowItem(itemName: "${String.fromCharCodes(Runes('\u0024'))}300"),
-                TableRowItem(itemName: "July 24, 2022"),
-              ]
-          ),
-          TableRow(
-              children: <Widget>[
-                TableRowItem(itemName: "Trip"),
-                TableRowItem(itemName:  "${String.fromCharCodes(Runes('\u0024'))}800"),
-                TableRowItem(itemName: "July 13, 2022"),
-              ]
-          ),
-          TableRow(
-              children: <Widget>[
-                TableRowItem(itemName: "Education"),
-                TableRowItem(itemName: "${String.fromCharCodes(Runes('\u0024'))}150"),
-                TableRowItem(itemName: "July 24, 2022"),
-              ]
-          ),
-        ],
+      margin: const EdgeInsets.only(top: 40),
+      child: StreamBuilder<QuerySnapshot>(
+        stream: FirebaseFirestore.instance.collection('expenses').snapshots(),
+        builder: (context, snapshot) {
+          if (!snapshot.hasData) {
+            return const Center(
+                child: CircularProgressIndicator()
+            );
+          }
+          final expenses = snapshot.data!.docs;
+
+          List<TableRow> rowWidgets = [];
+          List<TableRowItem> rowTableHeaderWidgets = [];
+          rowTableHeaderWidgets.add(const TableRowItem(itemName: 'Category', color: Colors.green, textColor: Colors.white));
+          rowTableHeaderWidgets.add(const TableRowItem(itemName: 'Price', color: Colors.green, textColor: Colors.white));
+          rowTableHeaderWidgets.add(const TableRowItem(itemName: 'Date', color: Colors.green, textColor: Colors.white));
+
+          rowWidgets.add(TableRow(children: rowTableHeaderWidgets));
+
+          for (var expense in expenses) {
+            List<TableRowItem> rowItemWidgets = [];
+
+            final category = expense.get('category');
+            final price = expense.get('price').toString();
+            final date = expense.get('date').toString();
+
+            rowItemWidgets.add(TableRowItem(itemName: category, color: Colors.white, textColor: Colors.green,));
+            rowItemWidgets.add(TableRowItem(itemName: price, color: Colors.white, textColor: Colors.green,));
+            rowItemWidgets.add(TableRowItem(itemName: date, color: Colors.white, textColor: Colors.green,));
+
+            rowWidgets.add(TableRow(children: rowItemWidgets));
+          }
+
+          return Table(
+            border: TableBorder.all(),
+            defaultVerticalAlignment: TableCellVerticalAlignment.middle,
+            children: rowWidgets,
+          );
+        },
       ),
     );
   }
 }
 
-class TableHeader extends StatelessWidget {
-
-  TableHeader({
-    required this.headerName
-  });
-
-  final String headerName;
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      alignment: Alignment.center,
-      width: 30,
-      height: 33,
-      color: Colors.green,
-      child: Text(headerName, style: const TextStyle(color: Colors.white),),
-    );
-  }
-}
-
 class TableRowItem extends StatelessWidget {
-
   const TableRowItem({Key? key,
-    required this.itemName
+    required this.itemName,
+    required this.color,
+    required this.textColor,
   }) : super(key: key);
 
   final String itemName;
+  final Color color;
+  final Color textColor;
 
   @override
   Widget build(BuildContext context) {
@@ -96,7 +77,8 @@ class TableRowItem extends StatelessWidget {
       alignment: Alignment.center,
       width: 30,
       height: 33,
-      child: Text(itemName, style: const TextStyle(color: Colors.green),),
+      color: color,
+      child: Text(itemName, style: TextStyle(color: textColor),),
     );
   }
 }
